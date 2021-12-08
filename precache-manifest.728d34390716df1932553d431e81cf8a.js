d1c6c0291939f5f9b97a788353a727e0f5493d55
Hana <momenthana@kakao.com>
2021-12-08 03:14:59 +0000

    init

---
+++ precache-manifest.728d34390716df1932553d431e81cf8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ab4749e1e92a19f07740ff2f50b6fd",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "4e341be47405f0ca386c",
+    "url": "/css/chunk-vendors.3ed875db.css"
+  },
+  {
+    "revision": "05387350db37ca90affe82595027f827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e856611ee7060f446892",
+    "url": "/js/app.05255499.js"
+  },
+  {
+    "revision": "4e341be47405f0ca386c",
+    "url": "/js/chunk-vendors.e678e175.js"
+  },
+  {
+    "revision": "3bee6610541281461bba2bfb317b66eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
